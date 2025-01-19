d235d473553dcb059bbfe716360ae05a733f0e2f
timo131 <thomasemosley3@gmail.com>
2025-01-19 20:03:54 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+    let subject = ["My dog", "My mother-in-law", "A pidgeon", "The boogieman"];
+    let action = ["pooped on", "pissed on", "crushed", "broke", "stole", "ate"];
+    let object = ["my face", "my car", "my dignity", "my underwear"];
+    let when = [
+      "before class.",
+      "this morning.",
+      "last night.",
+      "in the middle of the night.",
+      "on the way to school."
+    ];
+    let subjectIndex = Math.floor(Math.random() * subject.length);
+    let actionIndex = Math.floor(Math.random() * action.length);
+    let objectIndex = Math.floor(Math.random() * object.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+    return (
+      subject[subjectIndex] +
+      " " +
+      action[actionIndex] +
+      " " +
+      object[objectIndex] +
+      " " +
+      when[whenIndex]
+    );
+  }
+  let excuseText = generateExcuse();
+  document.querySelector("#excuse").innerHTML = excuseText;
+  console.log("Excuse:", excuseText);
 };
